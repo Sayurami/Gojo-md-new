0c88055e1471b10381a4fd5eccc05987795088eb
eransayura2 <sayuramihiranga790@gmail.com>
2025-07-30 20:33:46 +0530

    Delete settings.js directory

--- settings.js/session.js
+++
@@ -1,5 +0,0 @@
-if (require('fs').existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
-module.exports = {
-  SESSION_ID: process.env.SESSION_ID || '6gQRhYTS#Bv8B9AtMzA5x9SL1O8UIC6KvOvg8pecbi36RWrPLSUM'
-};
