5ada26d23984edc103c6d7c3258383ad1f7f675d
eransayura2 <sayuramihiranga790@gmail.com>
2025-07-30 20:49:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,6 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94743826406' : process.env.OWNER_NUMBER,  
-PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
 ALIVE:  process.env.ALIVE  || '> GOJO MD',
 GEMINI_API_KEY:process.env.GEMINI_API_KEY || 'AIzaSyDQIUl78aFtIgsNKY1RUU82nDkL905UbtA',
